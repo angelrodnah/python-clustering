d2c26640acf76cbf1ab544fec62b2603590429fa
Ivan Reznikov <ivanreznikov@gmail.com>
2022-07-13 01:17:35 +0400

    bump to 1.1.0

--- src/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-from pathlib import Path
-
-if __name__ == "__main__":
-    current_directory = Path(__file__).parent
-    setup(
-        long_description = (current_directory / "README.md").read_text(),
-        long_description_content_type="text/markdown"
-    )
